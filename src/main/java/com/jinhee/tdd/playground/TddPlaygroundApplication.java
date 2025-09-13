30f4537ed597a0377b04a4c166381e2d7ce37efd
jinheelee04 <2pro.jlee@gmail.com>
2025-09-13 09:29:53 +0900

    chore: Spring Boot 프로젝트 초기 생성 및 Gradle 설정 추가

---
+++ src/main/java/com/jinhee/tdd/playground/TddPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jinhee.tdd.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinhee/tdd/playground/TddPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinhee.tdd.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
