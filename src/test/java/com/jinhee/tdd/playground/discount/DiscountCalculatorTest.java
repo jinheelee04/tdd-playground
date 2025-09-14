019ed4de656ec4e94ea2f22eeb60193295a7e268
jinheelee04 <2pro.jlee@gmail.com>
2025-09-14 09:18:43 +0900

    test: BASIC 회원이 조조할인 시작 시간 6시에 예약시 할인율 5% 적용 테스트 추가

--- src/test/java/com/jinhee/tdd/playground/discount/DiscountCalculatorTest.java
+++ src/test/java/com/jinhee/tdd/playground/discount/DiscountCalculatorTest.java
@@ -31,6 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * 3. GOLD 회원 + 10시 30분 예약 -> 20% 할인
  * 4. BASIC 회원 + 6시 30분 예약 -> 5% 할인
  * 5. BASIC 회원 + 06:00 ~ 09:00 예약 -> 5% 할인
+ * 6. BASIC 회원 + 오전 6시 예약 -> 5% 할인
  */
 public class DiscountCalculatorTest {
 
@@ -98,5 +99,16 @@ public class DiscountCalculatorTest {
         assertEquals(expected, discounted);
     }
 
+    @Test
+    @DisplayName("BASIC 회원이 오전 6시에 예약하면 할인율 5%")
+    void  givenBasicMember_whenReserveAt6AM_thenApply5PercentDiscount(){
+        LocalTime reservationTime = LocalTime.of(6, 00);
+        int price = 10_000;
+
+        DiscountCalculator cal = new DiscountCalculator();
+        int discounted = cal.calculateDiscount(price, Membership.BASIC, reservationTime);
+        int expected = (int) Math.round(price * 0.95);
+        assertEquals(expected, discounted);
+    }
 
 }
